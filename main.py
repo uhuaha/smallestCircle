fa8ea1582fab0cf3391edd0862315c4c825b6808
stefan <stefanweiher@web.de>
2019-01-27 22:29:50 +0100

    A few changes in comments and in the README file

--- main.py
+++ main.py
@@ -3,16 +3,18 @@
 import welzlAlgorithm
 import draw
 
-# list of triangles in 3D, i.e., it is a list of points
+# List of triangles in 3D, i.e., it is a list of points
 allPoints = [{"a":3,"b":4,"c":1}, {"a":2,"b":1,"c":5}, {"a":2,"b":4,"c":4},
              {"a":2,"b":1,"c":3}, {"a":2,"b":2,"c":1}, {"a":1,"b":1,"c":1},
              {"a":2,"b":1,"c":2}, {"a":1,"b":1,"c":2}, {"a":3,"b":1,"c":4},]
 
-# dimension to skip - a, b or c - because we have to reduce the problem to a 2D problem
-dimToSkip = "c"
 
-# before we go into Welzl's algorithm, we need to convert the dictionaries into lists and we need to remove one dimension
-# in order to reduce the problem to a problem in 2D
+# Dimension to skip - a, b or c - because we have to reduce the 3D problem to a 2D problem
+dimToSkip = "a"
+
+
+# Before we go into Welzl's algorithm, we need to convert the dictionaries into lists and we need
+# to remove one dimension
 allPoints2D = []
 for points in allPoints:
     tempList = []
@@ -22,7 +24,9 @@ for points in allPoints:
     allPoints2D.append(tempList)
 print(allPoints2D)
 
-# Check for double points and remove them from the list
+
+# Check for double points and remove them from the list; if not removed these double points will cause problems
+# in the algorithm
 seen = []
 keep = []
 for point in allPoints2D:
@@ -33,7 +37,7 @@ for point in allPoints2D:
         keep.append(point)
 
 
-# Execute the algorithm until a defined smallest circle was found
+# Execute the algorithm until a defined smallest circle in a given plane was found
 circleFound = False
 while not circleFound:
     pointsToKeep = keep.copy()
@@ -50,7 +54,7 @@ while not circleFound:
     if circle:
         allPointsInside = welzlAlgorithm.checkAllPointsInside(circle, seen)
 
-    # Draw to check
+    # Visualize all points and the smallest circle
     if circle is not None and allPointsInside:
         circleFound = True
         draw.draw(circle, seen)
